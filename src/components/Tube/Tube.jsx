bb60662dc07d92744978a64896213b09fa2b43ba
Gregory Kirchoff <gckirchoff@gmail.com>
2021-04-15 16:51:27 -0400

    added media queries

--- src/components/Tube/Tube.jsx
+++ src/components/Tube/Tube.jsx
@@ -214,9 +214,10 @@ const Tube = () => {
   const handleSearchClick = async () => {
     try {
       const res = await axios.get(
-        `http://127.0.0.1:3005/api/v1/results/${finalCode}`
+        // `http://127.0.0.1:3005/api/v1/results/${finalCode}` for development
+        // `/api/v1/results/${finalCode}` relative url for SSR
+        `/api/v1/results/${finalCode}`
       );
-      console.log(res.data.data.result);
       setNotFound(false);
       setResult(res.data.data.result);
     } catch {
